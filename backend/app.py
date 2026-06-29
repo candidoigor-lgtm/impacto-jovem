3afa49b0aace34be105c85140fc06adc8d668d3f
igor_escola <candido.igor@escola.pr.gov.br>
2026-06-29 07:23:36 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/status', methods=['GET'])
-def get_status():
-    return jsonify({"status": "Backend rodando com sucesso!"})
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({
+        "projeto": "Impacto Jovem",
+        "status": "Online",
+        "mensagem": "API do sistema de Inteligência Artificial funcionando com sucesso!"
+    }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
